5a8ded640c9841110e6c7d2f23d5ada4edafec3d
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-11-18 16:56:00 -0300

    Enemy movement system was greatly enhanced

--- Assets/Resources/Scripts/EnemyMover.cs
+++ Assets/Resources/Scripts/EnemyMover.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
+    [Header("Travel Path")]
+    [Tooltip("The list of tiles that this enemy can move (path)")]
     [SerializeField] private List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] private float secondsEnemyMovement = 1f;
-    
+
+    [Header("Movement")]
+    [Tooltip("How fast this enemy can move")]
+    [SerializeField][Range(0, 5f)] private float movementSpeed = 1f;
+
     void Start()
     {
         StartCoroutine(this.FollowPath());
@@ -16,9 +21,29 @@ public class EnemyMover : MonoBehaviour
     private IEnumerator FollowPath()
     {
         foreach (Waypoint waypoint in path)
-        {            
-            this.transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(this.secondsEnemyMovement);
+        {
+            //Calculates where the enemy is going to move into
+            Vector3 startPosition = this.transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+
+            //Where in the movement is the enemy starting (Between the two
+            //points;0 is start and 1 is the end position)
+            float travelPercent = 0f;
+
+            //Makes the enemy rotates so its forward part points
+            //to the tile that it is going into
+            this.transform.LookAt(endPosition);
+
+            //While it still did not arrive at its destination, it
+            //should keep moving
+            while(travelPercent < 1f)
+            {
+                travelPercent += Time.deltaTime * this.movementSpeed;
+                //Makes the enemy goes into the destination gradatively
+                //and not at once
+                this.transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }            
         }
     }
 }
