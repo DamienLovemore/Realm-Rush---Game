889a12c92ff6ade599d513b4e611d7d8a41d99b4
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-11-19 14:35:10 -0300

    Now enemies have an AI to find and follow path automatically wihout having to know previously by code

--- Assets/Resources/Scripts/EnemyMover.cs
+++ Assets/Resources/Scripts/EnemyMover.cs
@@ -4,23 +4,49 @@ using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
-    [Header("Travel Path")]
-    [Tooltip("The list of tiles that this enemy can move (path)")]
-    [SerializeField] private List<Waypoint> path = new List<Waypoint>();
-
     [Header("Movement")]
     [Tooltip("How fast this enemy can move")]
     [SerializeField][Range(0, 5f)] private float movementSpeed = 1f;
 
+    private List<Waypoint> path = new List<Waypoint>();
+
     void Start()
     {
+        //Finds all the path to follow
+        this.FindPath();
+        //Teleport them to the first tile of the path
+        this.ReturnToStart();
+        //Starts following the path until the end
         StartCoroutine(this.FollowPath());
+    }
+
+    //Find the path for the enemy to follow whithout us having to set it
+    //manually in the editor, by SerializeField.
+    private void FindPath()
+    {
+        //The object that have all the children path inside it
+        //(Instead of finding all paths with the same tag cause 
+        //you cannot be sure if it will return in the same order)
+        GameObject pathFather = GameObject.FindGameObjectWithTag("Path");
+        Waypoint[] waypointsChildren = pathFather.GetComponentsInChildren<Waypoint>();
+        
+        //Adds all the path found for the enemy to follow later
+        foreach(Waypoint waypoint in waypointsChildren)
+        {            
+            path.Add(waypoint);
+        }
+    }
+
+    //Makes the enemy to appear in the first path position
+    private void ReturnToStart()
+    {
+        transform.position = this.path[0].transform.position;
     }
 
     //Function used for the enemy movement between the tiles
     private IEnumerator FollowPath()
     {
-        foreach (Waypoint waypoint in path)
+        foreach (Waypoint waypoint in this.path)
         {
             //Calculates where the enemy is going to move into
             Vector3 startPosition = this.transform.position;
@@ -45,5 +71,8 @@ public class EnemyMover : MonoBehaviour
                 yield return new WaitForEndOfFrame();
             }            
         }
+
+        //After following all the path, the enemy disappears
+        Destroy(this.gameObject);
     }
 }
