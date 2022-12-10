7a9ef078d62e7f798a9006634656a1fea05a4a3a
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-12-10 08:33:18 -0300

    Added increasing difficulty to the game

--- Assets/Resources/Scripts/EnemyHealth.cs
+++ Assets/Resources/Scripts/EnemyHealth.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Makes the component to be added automatically if
+//it is not found
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private int maxHitPoints = 5;
+    [SerializeField] private int difficultyRamp = 1;
 
     private int currentHitPoints;
     private  Enemy enemyPenalty;
@@ -35,10 +39,13 @@ public class EnemyHealth : MonoBehaviour
         //Takes damage
         this.currentHitPoints -= 1;
 
-        //the enemy is set back to the object pool
+        //the enemy is set back to the object pool, and the
+        //enemies health is slightly increased to increase
+        //the difficulty
         if (this.currentHitPoints == 0)
         {
             this.gameObject.SetActive(false);
+            maxHitPoints += this.difficultyRamp;
             this.enemyPenalty.RewardGold();
         }
     }
