a6b78686dae4436ed1f9a9f87b63440cf3ee78e6
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-12-12 19:52:53 -0300

    Ambience enhanced in the game level design

--- Assets/Resources/Scripts/Tower.cs
+++ Assets/Resources/Scripts/Tower.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    [SerializeField] private float buildDelay = 1f;
     [SerializeField] private int cost = 75;
 
+    void Start() 
+    {
+        StartCoroutine(this.Build());
+    }
+
+    //Creates a tower in a tile
     public bool CreateTower(Tower tower, Vector3 position)
     {
         //Finds the bank system to see if there is money
@@ -27,6 +34,35 @@ public class Tower : MonoBehaviour
         else
         {
             return false;
+        }
+    }
+
+    //Is called when the tower instantiates(creates) in its
+    //start so that its parts become visible over time, and
+    //not suddenly appear.
+    private IEnumerator Build()
+    {
+        //Gets all the children and grand-children of this tower
+        //because all of them have a Transform component
+        foreach (Transform child in this.transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandchild in child)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        //Enable the parts of the tower if a delay, showing
+        //that the tower is being built and not just appearing
+        foreach (Transform child in this.transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(this.buildDelay);
+            foreach (Transform grandchild in child)
+            {
+                child.gameObject.SetActive(true);
+            }
         }
     }
 }
